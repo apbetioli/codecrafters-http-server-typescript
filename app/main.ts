019648f7d1c88badde9b3a4be6198c3728b987ec
Alexandre Betioli <apbetioli@gmail.com>
2024-06-10 17:15:25 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,16 +8,41 @@ const echo = (path: string) => {
   return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
 };
 
+const root = () => {
+  return "HTTP/1.1 200 OK\r\n\r\n";
+};
+
+const notFound = () => {
+  return "HTTP/1.1 404 Not Found\r\n\r\n";
+};
+
+const userAgent = (headers: { [key: string]: string }) => {
+  const useragent = headers["User-Agent"];
+  return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${useragent.length}\r\n\r\n${useragent}`;
+};
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const path = data.toString().split(" ")[1];
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path.startsWith("/echo")) {
-      socket.write(echo(path));
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    const request = data.toString();
+    const parts = request.split("\r\n");
+    const [_method, path, _version] = parts[0].split(" ");
+    const headers: { [key: string]: string } = {};
+    for (let i = 1; i < parts.length - 2; i++) {
+      const [key, value] = parts[i].split(": ");
+      headers[key] = value;
     }
+
+    if (path === "/") {
+      var response = root();
+    } else if (path.startsWith("/echo")) {
+      var response = echo(path);
+    } else if (path === "/user-agent") {
+      var response = userAgent(headers);
+    } else {
+      var response = notFound();
+    }
+
+    socket.write(response);
     socket.end();
   });
 });
