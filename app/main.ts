b5682126ad9ebb57f94469f32e6c805e0fd0efe6
Alexandre Betioli <apbetioli@gmail.com>
2024-06-10 16:47:49 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,20 @@
 import * as net from "net";
 
+const echo = (path: string) => {
+  let str = path.replace("/echo", "");
+  if (str.startsWith("/")) {
+    str = str.replace("/", "");
+  }
+  return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
+};
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const path = data.toString().split(" ")[1];
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.startsWith("/echo")) {
+      socket.write(echo(path));
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
